c2f097206ee76a7cafa09aa96ceead7a741f3203
bsc-phy-26-19 <95232639+bsc-phy-26-19@users.noreply.github.com>
2023-12-10 20:15:01 -0800

    Update MainActivity.java

--- app/src/main/java/com/nasiar/getfurniture/MainActivity.java
+++ app/src/main/java/com/nasiar/getfurniture/MainActivity.java
@@ -1,25 +1,14 @@
 package com.nasiar.getfurniture;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
 
-    Button login;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        login = findViewById(R.id.login);
-        login.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-            startActivity(intent);
-        });
     }
 }
